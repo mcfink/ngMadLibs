70a6ae14ed07d45d2f06bf46139ae9078f01c518
Mike <mcfink@gmail.com>
2015-03-18 10:04:50 -0400

    test commit

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 angular.module('myApp', [])
-.controller('oneController', ['$scope', function($scope) {
-
-}]);
+		.controller('myController', function($scope) {
+		var words = ["*female name*", "*job title*", "*tedious task*", "*dirty task*", "*celebrity*", "*useless skill*", "*adjective*", "*obnoxious celebrity*", "*huge number*" ];
+		$scope.words = words;
+	});
