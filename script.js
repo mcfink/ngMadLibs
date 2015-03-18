0ae5e7304cccf5a33986ed6cfe0974fd9c4a8b29
Mike <mcfink@gmail.com>
2015-03-18 16:09:01 -0400

    madlibs controller refactor #1 with gender specificity

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 angular.module('myApp', [])
 		.controller('myController', function($scope) {
-		var words = ["*female name*", "*job title*", "*tedious task*", "*dirty task*", "*celebrity*", "*useless skill*", "*adjective*", "*obnoxious celebrity*", "*huge number*" ];
+		var words = ["*female name*", "*job title*", "*tedious task*", "*dirty task*", "*celebrity*", "*useless skill*", "*adjective*", "*obnoxious celebrity*", "*huge number*", "*male name*" ];
+		var gender;
 		$scope.words = words;
+		$scope.gender = gender;
 	});
